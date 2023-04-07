1beeb2b1d1c1f143ca6c8818d97fd2eab31d74fd
Your Name <pavlikkim200401@gmail.com>
2023-04-07 08:11:10 +0600

    [ADDED] email check method

--- internal/services/algos/email_check.go
+++ internal/services/algos/email_check.go
@@ -1,1 +1,23 @@
 package algos
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func (s *AlgosService) EmailCheck(str string) []string {
+	allowedSymbols := "a-zA-Z.-_"
+	emailPattern := fmt.Sprintf(`Email:\s*([%s]+@[%s]+\.[%s])+`, allowedSymbols, allowedSymbols, allowedSymbols)
+	emailPatternRegex := regexp.MustCompile(emailPattern)
+
+	matches := emailPatternRegex.FindAllString(str, -1)
+	emails := make([]string, 0, len(matches))
+	for _, match := range matches {
+		buffer := strings.TrimPrefix(match, "Email:")
+		buffer = strings.TrimSpace(buffer)
+		emails = append(emails, buffer)
+	}
+
+	return emails
+}
